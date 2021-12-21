23fe698788257043085151653522eb53f7871c70
2847411749 <2843741749@qq.com>
2021-12-21 15:40:43 +0800

    分支

--- src/test.java
+++ src/test.java
@@ -1,7 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("1111111111111111111111");
-        System.out.println("在github添加了一条信息");
-
+        System.out.println("Fenzhi");
     }
 }
