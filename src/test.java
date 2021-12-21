e478e4706c5ce027563d0ee69418fad132fe236b
2847411749 <2843741749@qq.com>
2021-12-21 14:44:36 +0800

    剩下了一条信息

--- src/test.java
+++ src/test.java
@@ -1,8 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("1111111111111111111111");
-        System.out.println("在github添加了一条信息");
-        System.out.println("github");
-        System.out.println("在github添加了一条信息");
+
     }
 }
