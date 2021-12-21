33f5c50bf6caf5a5c816f7f3d87c79aee575f698
2847411749 <2843741749@qq.com>
2021-12-21 14:31:56 +0800

    在idea添加了一条消息

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("1111111111111111111111");
         System.out.println("在github添加了一条信息");
+        System.out.println("在idea添加了一条消息");
     }
 }
