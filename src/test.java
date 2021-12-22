075eb33ac946444c14ec057b1ef14f518ab14bd7
2847411749 <2843741749@qq.com>
2021-12-22 09:33:31 +0800

    分支

--- src/test.java
+++ src/test.java
@@ -1,8 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("1111111111111111111111");
         System.out.println("Fenzhi");
-        System.out.println("Fenzhi");
-
+        System.out.println("4232323");
     }
 }
